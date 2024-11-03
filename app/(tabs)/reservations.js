218c4172aab9f58460d406d4c1a7d3bf2d567e21
DevEwerton <ton.14@hotmail.com>
2024-11-03 12:04:35 -0300

    Updated 2024-11-03 Working on reservations view

--- app/(tabs)/reservations.js
+++ app/(tabs)/reservations.js
@@ -1,20 +1,75 @@
-import { StyleSheet, View, Text } from "react-native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 import { COLORS, HEIGHT_HEADER } from "../../constants";
 import Header from "../../components/Header";
+import ItemListReservation from "../../components/ItemListReservation";
+
+var RESERVATIONS = [
+	{
+		id: 1,
+		nameOwner: "Ewerton",
+		nameAnimal: "Milu",
+		phone: "11 95999-8098",
+		type: "DOG",
+		when: 1733238000000,
+		status: 1, //1 for scheduled, 2 for finished
+	},
+	{
+		id: 2,
+		nameOwner: "Maria",
+		nameAnimal: "Patty",
+		phone: "11 95999-8098",
+		type: "CAT",
+		when: 1733238000000,
+		status: 2, //1 for scheduled, 2 for finished
+	},
+]
 
 export default function reservations (props)
 {
+	const [reservations, setReservations] = useState(RESERVATIONS);
+
 	useEffect(() => 
 	{
 		
 	}, [props]);
+
+	async function onEditReservation ()
+	{
+
+	}
+
+	async function onConfirmRemoveReservation ()
+	{
+
+	}
 
 	return (
 		<View style={styles.container}>
 			<Header />
 			<View style={styles.body}>
 				<Text style={styles.title}>Reservas</Text>
+				<ScrollView
+					style={styles.scrollView}
+					contentContainerStyle={{alignItems: "center"}}
+					showsVerticalScrollIndicator={false}
+					showsHorizontalScrollIndicator={false}
+				>
+					{
+						reservations.map(p => {
+							return (
+								<ItemListReservation
+									key={p.id}
+									{...p}
+									onEditReservation={onEditReservation}
+									onRemoveReservation={onConfirmRemoveReservation}
+									// options={(p.idOwner === user?.id_user)}
+									options={true}
+								/>
+							)
+						})
+					}
+				</ScrollView>
 			</View>
 		</View>
 	);
@@ -35,5 +90,11 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		textAlign: "center",
 		color: COLORS.darkOne
-	}
+	},
+	scrollView: {
+        paddingTop: 30,
+        width: "100%",
+        height: "100%",
+        position: "relative",
+    },
 });
---
+++ components/ItemListReservation.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { COLORS, SIZES } from "../constants";
+
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Button from "./Button";
+
+export default function ItemListReservation (props)
+{
+    const [showOptions, setShowOptions] = useState(false);
+
+    return (
+            <View style={styles.container}>
+                <View style={styles.colOne}>
+                    <MaterialCommunityIcons name={props.type.toLowerCase()} style={styles.icon}/>
+                </View>
+                <View style={styles.colTwo}>
+                    <Text style={styles.title}>{props.nameAnimal}</Text>
+                    <Text style={styles.subtitle}>Para: {props.when}</Text>
+                    <Text style={styles.subtitle}>Dono: {props.nameOwner}</Text>
+                    <Text style={styles.subtitle}>Telefone: {props.phone}</Text>
+                    {
+                        props.options &&
+                        <Button
+                            label="..."
+                            style={styles.buttonShowOptions}
+                            onPress={() => setShowOptions(!showOptions)}
+                        />
+                    }
+                    {
+                        showOptions &&
+                        <View style={styles.options}>
+                            <Button
+                                label="alterar"
+                                style={styles.buttonOption}
+                                styleLabel={styles.labelButton}
+                                onPress={() => {
+                                    props.onEditPetshop(props);
+                                    setShowOptions(false);
+                                }}
+                            />
+                            <Button
+                                label="excluir"
+                                style={styles.buttonOption}
+                                styleLabel={styles.labelButton}
+                                onPress={() => {
+                                    props.onRemovePetshop(props);
+                                    setShowOptions(false);
+                                }}
+                            />
+                        </View>
+                    }
+                </View>
+                {props.status === 2 && <View style={styles.filter}></View>}
+            </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        minHeight: 240,
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: COLORS.lightOne,
+        borderRadius: 10,
+        marginVertical: 5,
+        shadowColor: "#000",
+        shadowOffset: { width: 5, height: 10 },
+        shadowOpacity: 0.8,
+        shadowRadius: 10,  
+        elevation: 2,
+        position: "relative"
+    },
+    filter: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        backgroundColor: "#28a745",
+        opacity: 0.3,
+        borderRadius: 10,
+    },
+    colOne: {
+        width: "40%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        verticalAlign: "middle",
+        textAlign: "center"
+    },
+    colTwo: {
+        width: "60%",
+        display: "flex",
+        justifyContent: "center",
+        position: "relative"
+    },
+    services: {
+        height: 40
+    },
+    icon: {
+        fontSize: 100,
+        color: COLORS.darkTwo,
+    },
+    title: {
+        fontSize: SIZES.title,
+        color: COLORS.darkTwo,
+        fontWeight: "600"
+    },
+    subtitle: {
+        fontSize: SIZES.subtitle,
+        color: COLORS.darkTwo,
+        fontWeight: "400",        
+    },
+    buttonShowOptions: {
+        position: "absolute",
+        top: -20,
+        right: -10,
+        width: 30,
+        height: 10,
+        backgroundColor: "transparent",
+        borderWidth: 0
+    },
+    options: {
+        position: "absolute",
+        right: 55,
+        top: 5,
+        width: 140,
+        display: "flex",
+        justifyContent: "center",
+        minHeight: 100,
+        backgroundColor: COLORS.lightTwo,
+        borderRadius: 5,
+    },
+    buttonOption: {
+        minHeight: 30,
+        margin: 5,
+        padding: 0
+    },
+    labelButton: {
+        fontSize: SIZES.description
+    }
+})
